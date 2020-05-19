a7a1b8ff7327ebb1b14b779055bf58590ec3ce77
Laurie Wandle <57124273+laurie-wandle@users.noreply.github.com>
2020-05-19 12:52:36 -0700

    Update db.inc.html.php

--- aceinthehole/includes/db.inc.html.php
+++ aceinthehole/includes/db.inc.html.php
@@ -1,14 +1,12 @@
+
 <?php
-try
-{
-    $pdo = new PDO('mysql:host=localhost;dbname=aceinthehole', 'ace_user', 'myAcePassword222');
+
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=lauriew1_aceinthehole', 'lauriew1_ace_user', 'myAcePassword222');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec('SET NAMES "utf8"');
-}
-catch (PDOException $e)
-{
+} catch (PDOException $e) {
     $error = 'Unable to connect to the database server.';
     include 'error.html.php';
     exit();
 }
-?>
