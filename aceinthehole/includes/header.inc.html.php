e1af7444d990c5d3d464a9659ba2f2d8930993d4
Laurie Wandle <57124273+laurie-wandle@users.noreply.github.com>
2020-06-01 12:34:50 -0700

    Update header.inc.html.php

--- aceinthehole/includes/header.inc.html.php
+++ aceinthehole/includes/header.inc.html.php
@@ -1,7 +1,15 @@
 <?php define('BASE_URL', '/cas222/aceinthehole/');?>
+
+
+<div id="links">
+    <a href="https://www.facebook.com/Cas222Aceinthehole-110661963841617/"><i class="fab fa-facebook fa-2x"></i></a>
+    <a href="https://twitter.com/pcccas222?ref_src=twsrc%5Etfw"> <i class="fab fa-twitter fa-2x"></i></a>
+</div>
+
 <header>
 
+    <h1 id="logo" onclick="window.location.href='http://lauriewandle1030.webhostingforstudents.com/cas222/aceinthehole/home/home.html.php'"><img src="<?echo BASE_URL;?>/images/ace_logo2.png" alt="Ace Hole in One logo"></h1>
 
-    <h1><img src="<? echo BASE_URL;?>images/ace_logo.png" alt="Ace Hole in One logo"></h1>
+
 
 </header>
